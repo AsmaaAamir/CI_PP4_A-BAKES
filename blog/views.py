30f064b56e29466dfc49c6cf5975953c59127241
AsmaaAamir <asma.liaqat@outlook.com>
2023-04-02 21:35:38 +0000

    added form py for comment on recipe

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -27,13 +28,14 @@ class PostDetail(View):
             liked = True
 
         return render(
-            request, 
+            request,
             "recipe.html",
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
-            }
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
         )
 
 
