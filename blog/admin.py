da44cdb797f427fe275908cd4f933d0f6877e335
AsmaaAamir <asma.liaqat@outlook.com>
2023-04-02 14:44:48 +0000

    added approved comment section code

--- blog/admin.py
+++ blog/admin.py
@@ -21,3 +21,7 @@ class CommentAdmin(SummernoteModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ['name', 'email', 'body']
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        queryset.update(approve=True)
