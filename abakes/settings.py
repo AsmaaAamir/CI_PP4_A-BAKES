a02675ca1c39de8d92abe4865ec10c61520986b1
AsmaaAamir <asma.liaqat@outlook.com>
2023-04-02 14:47:46 +0000

    added the html file to templates

--- abakes/settings.py
+++ abakes/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
