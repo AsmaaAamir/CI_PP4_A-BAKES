137494336cffdc4733aa0114ab7607cef484e03c
AsmaaAamir <asma.liaqat@outlook.com>
2023-04-02 13:54:05 +0000

    created model for post and comment section

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+"""
+    This is the Model for the recipe post. 
+"""
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    featured_image = CloudinaryField('image', default='blog_image')
+    excerpt = models.TextField(blank=True)
+    ingredient_content = models.TextField()
+    instruction_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+""" This is a modle for comments section for the post. """
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"comment {self.body} by {self.name}"
