ea33cd5c4b5a7c9a2491a7b0eb0489c38e08f8ae
AsmaaAamir <asma.liaqat@outlook.com>
2023-04-03 08:47:12 +0000

    finihed styling home page

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,4 @@
+//Timer for the message on actions //
 setTimeout(function (){
     let messages = document.getElementById("msg");
     let alert = new bootstrap.Alert(messages);
@@ -5,3 +6,19 @@ setTimeout(function (){
     }, 5000
     );
 
+    
+//Jave script for nav bar//
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+    
+const toggleNavbarVisibility = () => {
+    isNavbarExpanded = !isNavbarExpanded;
+    navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+    
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+    
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
