3493234efbffbbe98a46362c1249bee8ba2a7fa1
AsmaaAamir <asma.liaqat@outlook.com>
2023-07-14 06:31:38 +0000

    added the env file again with it details

--- blog/forms.py
+++ blog/forms.py
@@ -8,22 +8,6 @@ from django import forms
 from .models import Comment, Post
 
 
-class AddPostForm(forms.ModelForm):
-    """
-    Form to allow user to add recipes
-    """
-    excerpt = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': '5'
-    }))
-
-    class Meta:
-        """
-        defines a recipe post form meta include
-        """
-        model = Post
-        fields = ('title', 'featured_image', 'excerpt', 'ingredient_content', 'featured_image2', 'insuturction_content', 'featuren_image3')
-
-
 class CommentForm(forms.ModelForm):
     """
     created form so user can add comment via this form
